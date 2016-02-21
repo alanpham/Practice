7e1bf5174eec47f4a77818b05259021e21a92099
Alan Pham <pham.alan10@gmail.com>
2016-02-21 14:40:17 -0800

    works, but doesn't account for empty list yet.

--- Data Structures/Stack.cpp
+++ Data Structures/Stack.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 template <class T>
-class Stack{
+class StackLL{
     struct Node{
         T data;
         Node *next;
@@ -15,7 +15,7 @@ class Stack{
     Node *head;
     T *data;
 public:
-    Stack();
+    StackLL();
     void push(T);
     T& pop();
     T& peek();
@@ -23,38 +23,82 @@ public:
     void clear();
 };
 
-template <class T> Stack<T>::Stack() : size(0), head(NULL), data(new T()){
+template <class T> StackLL<T>::StackLL() : size(0), head(NULL), data(new T()){
 }
 
-template <class T> void Stack<T>::push(T data){
-    size++;
-    head->data = data;
-    head->next = NULL;
-}
-
-template <class T> T & Stack<T>::pop(){
-    T a;
-    if (size == 0){
-        return -1;
+template <class T> void StackLL<T>::push(T data){
+    Node * newHead = new Node;
+    if (head == NULL){
+        newHead->data = data;
+        newHead->next = NULL;
+    } 
+    else{
+        newHead->data = data;
+        newHead->next = head;
     }
-    return a;
+
+    size++;
+    head = newHead;
 }
 
-template <class T> T & Stack<T>::peek(){
-    if (size == 0)
-        return -1;
+template <class T> T & StackLL<T>::pop(){
+    Node * newHead = new Node;
+    Node * temp = new Node;
+    temp = head;
+
+    if (head->next == NULL){
+        /*return -1;*/
+    }
+    else{
+        newHead->data = head->next->data;
+        newHead->next = head->next->next;
+    }
+
+    size--;
+    head = newHead;
+
+    return temp->data;
+}
+
+template <class T> T & StackLL<T>::peek(){
     return head->data;
 }
 
-template <class T> bool Stack<T>::isEmpty(){
+template <class T> bool StackLL<T>::isEmpty(){
     return size == 0;
 }
 
-template <class T> void Stack<T>::clear(){
+template <class T> void StackLL<T>::clear(){
 }
 
 int main(){
-    Stack<double> stk;
-    stk.push(12.0);
+    StackLL<double> stk;
+    stk.push(12);
+    stk.push(13);
+    stk.push(14);
+    std::cout << "STACK 1" << std::endl;
+    double temp = stk.pop();
+    std::cout << temp << std::endl;
+    temp = stk.pop();
+    std::cout << temp << std::endl;
+    std::cout << stk.peek() << std::endl;
+    std::cout << stk.peek() << std::endl;
+    std::cout << stk.peek() << std::endl;
+    std::cout << stk.pop() << std::endl;    
+
+    StackLL<int> stk2;
+    stk2.push(1);
+    stk2.push(2);
+    stk2.push(3);
+    std::cout << "STACK 2" << std::endl;
+    double temp2 = stk2.pop();
+    std::cout << temp2 << std::endl;
+    temp2 = stk2.pop();
+    std::cout << temp2 << std::endl;
+    std::cout << stk2.peek() << std::endl;
+    std::cout << stk2.peek() << std::endl;
+    std::cout << stk2.peek() << std::endl;
+    std::cout << stk2.pop() << std::endl;
+
     return 0;
 }
