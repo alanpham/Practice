fc46199f8dcfd16435e544a77e83b02e4009f856
Alan Pham <pham.alan10@gmail.com>
2016-02-21 12:15:48 -0800

    removes all previous code in Stack.cpp to make way for using Templates

--- Data Structures/Stack.cpp
+++ Data Structures/Stack.cpp
@@ -11,9 +11,7 @@
 #include <iostream>
 
 class Stack{
-    int *data;
-    int top;
-    int capacity;
+
 public:
     Stack();
     void push(int);
@@ -22,68 +20,37 @@ public:
     bool isFull();
     bool isEmpty();
     void resize();
-    void clear();
+    ~Stack();
 };
 
 Stack::Stack(){
-    capacity = 16;
-    top = -1;
-    data = new int[capacity];
 }
 
 void Stack::push(int num){
-    if (top == -1)
-        return -1;
-
-    top++;
-    if (top == capacity)
-        resize();
-
-    data[top] = num;
 }
 
 int Stack::pop(){
-    if (top == -1)
-        return -1;
-
-    int result = data[top];
-    data[top] = 0;
-    top -= 1;
-
-    return result;
+    return 0;
 }
 
 int Stack::peek(){
-    return data[top];
+    return 0;
 }
 
 bool Stack::isFull(){
-    if (top == capacity)
-        return true;
-
     return false;
 }
 
 bool Stack::isEmpty(){
-    if (top == 0)
-        return true;
-
     return false;
 }
 
 void Stack::resize(){
-
 }
 
 void Stack::clear(){
 }
 
 int main(){
-    Stack *stk = new Stack();
-    stk.push(1);
-    stk.push(2);
-    stk.push(3);
-
-
     return 0;
 }
