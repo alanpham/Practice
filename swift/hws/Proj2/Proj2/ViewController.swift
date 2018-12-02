bb54913716d52a04a5f33f337a7d9c410ea910ee
Alan Pham <pham.alan10@gmail.com>
2018-12-02 15:29:04 -0800

    removes some commented code, uses UILabel to display score instead of UIAlertController

--- swift/hws/Proj2/Proj2/ViewController.swift
+++ swift/hws/Proj2/Proj2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
   @IBOutlet var button1: UIButton!
   @IBOutlet var button2: UIButton!
   @IBOutlet var button3: UIButton!
+  @IBOutlet var uiLabelScore: UILabel!
   
   var countries = [String]()
   var correctAnswer = 0
@@ -21,28 +22,15 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-//    countries.append("estonia")
-//    countries.append("france")
-//    countries.append("germany")
-//    countries.append("ireland")
-//    countries.append("italy")
-//    countries.append("monaco")
-//    countries.append("nigeria")
-//    countries.append("poland")
-//    countries.append("russia")
-//    countries.append("spain")
-//    countries.append("uk")
-//    countries.append("us")
-    
     countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     
     button1.layer.borderWidth = 1
     button2.layer.borderWidth = 1
     button3.layer.borderWidth = 1
     
-    button1.layer.borderColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0).cgColor //UIColor.lightGray.cgColor
-    button2.layer.borderColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0).cgColor //UIColor.lightGray.cgColor
-    button3.layer.borderColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0).cgColor //UIColor.lightGray.cgColor
+    button1.layer.borderColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0).cgColor
+    button2.layer.borderColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0).cgColor
+    button3.layer.borderColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0).cgColor
     
     askQuestion()
   }
@@ -56,11 +44,10 @@ class ViewController: UIViewController {
     button3.setImage(UIImage(named: countries[2]), for: .normal)
     
     correctAnswer = Int.random(in: 0...2)
-    title = countries[correctAnswer].uppercased()
+    //title = countries[correctAnswer].uppercased()
   }
 
   @IBAction func buttonTapped(_ sender: UIButton) {
-    var title: String
     
     if sender.tag == correctAnswer {
       title = "Correct"
@@ -71,10 +58,12 @@ class ViewController: UIViewController {
       score -= 1
     }
     
-    let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
-    
-    ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-    present(ac, animated: true)
+//    let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+//
+//    ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+//    present(ac, animated: true)
+    uiLabelScore.text = "Your Score is: \(score)"
+    askQuestion()
   }
   
   
